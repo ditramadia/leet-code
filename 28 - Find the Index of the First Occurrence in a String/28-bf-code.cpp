0ba1e2220a8122200e8141bfc1e6c9a280c9dbad
ditramadia <ditrarizqaamadia@gmail.com>
2024-03-24 00:29:42 +0700

    problem: add 28 - Find the Index of the First Occurence in a String

---
+++ 28 - Find the Index of the First Occurrence in a String/28-bf-code.cpp
@@ -0,0 +1,38 @@
+#include <string>
+
+/**
+ * Find the Index of the First Occurrence in a String
+ * Brute force approach
+ * https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+ *
+ * Created: 2024-03-24
+ * Updated: 2024-03-24
+ * Author: Ditra Amadia
+ *
+ * O(m*n) time complexity
+ * 3ms runtime
+ */
+
+class Solution
+{
+public:
+  int strStr(std::string haystack, std::string needle)
+  {
+    for (int i = 0; i < haystack.length(); i++)
+    {
+      for (int j = 0; j < needle.length(); j++)
+      {
+        if (needle[j] != haystack[i + j])
+        {
+          break;
+        }
+
+        if (j == needle.length() - 1)
+        {
+          return i;
+        }
+      }
+    }
+    return -1;
+  }
+};
